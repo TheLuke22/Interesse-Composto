7c87d821d638ee5f22cb8827123899b8e6d372c8
TheLuke22 <lukeciccarelli30@gmail.com>
2026-08-21 19:49:52 +0200

    fix(imports): correct function names in components/__init__.py for Streamlit Cloud

--- components/__init__.py
+++ components/__init__.py
@@ -3,13 +3,14 @@ UI components package for Streamlit rendering, charts, sensitivity tables, and g
 """
 
 from components.dcf_ui import render_dcf_valuation_ui
-from components.portfolio_ui import render_portfolio_analytics_ui
-from components.compound_ui import render_compound_calculator_ui
+from components.portfolio_ui import render_portfolio_dividend_suite, render_portfolio_stress_test_suite
+from components.compound_ui import render_compound_interest_ui
 from components.consensus_pe_ui import render_consensus_pe_section
 
 __all__ = [
     "render_dcf_valuation_ui",
-    "render_portfolio_analytics_ui",
-    "render_compound_calculator_ui",
+    "render_portfolio_dividend_suite",
+    "render_portfolio_stress_test_suite",
+    "render_compound_interest_ui",
     "render_consensus_pe_section",
 ]
